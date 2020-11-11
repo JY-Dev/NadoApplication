7b8f161dff3e00349799fc09bc966168b36fafd7
Jykim <kjy@geosoft.co.kr>
2020-11-11 16:40:39 +0900

    - Guide 작업시작

---
+++ app/src/main/java/com/jydev/nadoapplication/GuideActivity.kt
@@ -0,0 +1,12 @@
+package com.jydev.nadoapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guide)
+
+    }
+}
